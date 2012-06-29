57551f2565c2b92c048dd991d338608a3355a524
Bjarte Johansen <bjo013@uib.no>
2012-06-29 17:53:43 +0200

    Sniffing events in os x through the Cocoa api
    Now we can log in OS X as well. Still have to connect it to the rest of
    the program. Will only print to stdout right now.

---
+++ sniff_cocoa.py
@@ -0,0 +1,72 @@
+from Cocoa import (NSApp,
+                   NSEvent,
+                   NSKeyDownMask, 
+                   NSLeftMouseUp, NSLeftMouseDown, NSLeftMouseUpMask, NSLeftMouseDownMask,
+                   NSRightMouseUp, NSRightMouseDown, NSRightMouseUpMask, NSRightMouseDownMask,
+                   NSMouseMoved, NSMouseMovedMask,
+                   NSScrollWheel, NSScrollWheelMask)
+from Foundation import NSObject, NSApplication, NSLog
+from PyObjCTools import AppHelper
+
+
+class SniffCocoa:
+    def __init__(self):
+        self.key_hook = lambda x: True
+        self.mouse_button_hook = lambda x: True
+        self.mouse_move_hook = lambda x: True
+
+    def createAppDelegate (self) :
+        sc = self
+        class AppDelegate(NSObject):
+            def applicationDidFinishLaunching_(self, notification):
+                mask = (NSKeyDownMask 
+                        | NSLeftMouseDownMask 
+                        | NSLeftMouseUpMask
+                        | NSRightMouseDownMask 
+                        | NSRightMouseUpMask
+                        | NSMouseMovedMask 
+                        | NSScrollWheelMask)
+                NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(mask, sc.handler)
+        return AppDelegate
+
+    def run(self):
+        app = NSApplication.sharedApplication()
+        delegate = self.createAppDelegate().alloc().init()
+        NSApp().setDelegate_(delegate)
+        AppHelper.runEventLoop()
+
+    def cancel(self):
+        AppHelper.stopEventLoop()
+    
+    def handler(self, event):
+        NSLog(u"%@", event)
+        """
+        if event.type == NSLeftMouseDown:
+           self.mouse_button_hook(1, True)
+        elif event.type == NsLeftMouseUp:
+           self.mouse_button_hook(1, False)
+        elif event.type == NSRightMouseDown:
+           self.mouse_button_hook(2, True)
+        elif event.type == NSRightMouseUp:
+           self.mouse_button_hook(2, False)
+        elif event.type == NSKeyDown:
+           self.key_hook(event.keyCode, None, event.characters, True, event.isARepeat)
+        elif event.type == NSMouseMoved:
+            loc = NSEvent.mouseLocation
+            self.mouse_move_hook(loc.x, loc.y)
+        """
+
+    def key_event(self, event):
+        return event.detail, event.state, self.get_key_name(event.detail, event.state), event.type == X.KeyPress, event.sequence_number == 1
+    
+    def button_event(self, event):
+        return event.detail, True
+
+    def lookup_keysym(self, keysym):
+        if keysym in self.keysymdict:
+            return self.keysymdict[keysym]
+        return "[%d]" % keysym
+
+    
+
+

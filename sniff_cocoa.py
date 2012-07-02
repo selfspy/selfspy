87c41c970f1951c263f2868e8271304990764c92
Bjarte Johansen <bjo013@uib.no>
2012-07-02 23:01:30 +0200

    OS X:added modifier support, window size support

--- sniff_cocoa.py
+++ sniff_cocoa.py
@@ -5,7 +5,8 @@ from Cocoa import (NSEvent,
                    NSLeftMouseUp, NSLeftMouseDown, NSLeftMouseUpMask, NSLeftMouseDownMask,
                    NSRightMouseUp, NSRightMouseDown, NSRightMouseUpMask, NSRightMouseDownMask,
                    NSMouseMoved, NSMouseMovedMask,
-                   NSScrollWheel, NSScrollWheelMask)
+                   NSScrollWheel, NSScrollWheelMask,
+                   NSAlternateKeyMask, NSCommandKeyMask, NSControlKeyMask)
 from PyObjCTools import AppHelper
 
 class SniffCocoa:
@@ -14,7 +15,6 @@ class SniffCocoa:
         self.mouse_button_hook = lambda x: True
         self.mouse_move_hook = lambda x: True
         self.screen_hook = lambda x: True
-      
 
     def createAppDelegate (self) :
         sc = self
@@ -47,16 +47,13 @@ class SniffCocoa:
                                                         kCGNullWindowID)
                 for window in windowList:
                     if window['kCGWindowNumber'] == windowNumber:
-                        process_name = window['kCGWindowOwnerName']
-                        window_name = window['kCGWindowName']
-                        # BELOW IS NOT CORRECT, NEED TRANSFORM
                         geometry = window['kCGWindowBounds'] 
-                        screen_hook(process_name, 
-                                    window_name, 
-                                    geometry.x, 
-                                    geometry.y, 
-                                    geometry.width, 
-                                    geometry.height)
+                        screen_hook(window['kCGWindowOwnerName'],
+                                    window['kCGWindowName'],
+                                    geometry['X'], 
+                                    geometry['Y'], 
+                                    geometry['Width'], 
+                                    geometry['Height'])
                         break
             if event.type() == NSLeftMouseDown:
                 loc = NSEvent.mouseLocation()
@@ -68,9 +65,18 @@ class SniffCocoa:
                 self.mouse_button_hook(3, loc.x, loc.y, True)
 #           elif event.type() == NSRightMouseUp:
 #               self.mouse_button_hook(2, False)
+            elif event.type() == NSScrollWheel:
+                # Scroll behaves differently on OS X then in Xorg, need to think of something here
             elif event.type() == NSKeyDown:
+                modifiers = [] # OS X api doesn't care it if is left or right
+                if (flags & NSControlKeyMask):
+                    modifiers.append('CONTROL')
+                if (flags & NSAlternateKeyMask):
+                    modifiers.append('ALTERNATE')
+                if (flags & NSCommandKeyMask):
+                    modifiers.append('COMMAND')
                 self.key_hook(event.keyCode(), 
-                              NSEvent.modifierFlags(), #This is not correct, need to transform
+                              modifiers,
                               event.characters(), 
                               True, 
                               event.isARepeat())

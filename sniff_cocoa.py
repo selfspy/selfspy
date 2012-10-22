e516eeee578782ee3bbef91e389461196439252d
Bjarte Johansen <bjo013@uib.no>
2012-10-22 17:51:42 +0200

    added keycode dict according to how activity should recieve

--- sniff_cocoa.py
+++ sniff_cocoa.py
@@ -7,7 +7,8 @@ from Cocoa import (NSEvent,
                    NSMouseMoved, NSMouseMovedMask,
                    NSScrollWheel, NSScrollWheelMask,
                    NSFlagsChanged, NSFlagsChangedMask,
-                   NSAlternateKeyMask, NSCommandKeyMask, NSControlKeyMask)
+                   NSAlternateKeyMask, NSCommandKeyMask, NSControlKeyMask,
+                   NSShiftKeyMask, NSAlphaShiftKeyMask)
 from Quartz import CGWindowListCopyWindowInfo, kCGWindowListOptionOnScreenOnly, kCGNullWindowID
 from PyObjCTools import AppHelper
 
@@ -67,6 +68,7 @@ class SniffCocoa:
                                              geometry['Height'])
                             break
                     break
+
             loc = NSEvent.mouseLocation()
             if event.type() == NSLeftMouseDown:
                 self.mouse_button_hook(1, loc.x, loc.y)
@@ -92,15 +94,25 @@ class SniffCocoa:
             elif event.type() == NSKeyDown:
                 flags = event.modifierFlags()
                 modifiers = [] # OS X api doesn't care it if is left or right
-                if (flags & NSControlKeyMask):
-                    modifiers.append('CONTROL')
-                if (flags & NSAlternateKeyMask):
-                    modifiers.append('ALTERNATE')
-                if (flags & NSCommandKeyMask):
-                    modifiers.append('COMMAND')
+                if flags & NSControlKeyMask:
+                    modifiers.append('Ctrl')
+                if flags & NSAlternateKeyMask:
+                    modifiers.append('Alt')
+                if flags & NSCommandKeyMask:
+                    modifiers.append('Cmd')
+                if flags & (NSShiftKeyMask | NSAlphaShiftKeyMask):
+                    modifiers.append('Shift')
+                character = event.charactersIgnoringModifiers()
+                # these two get a special case because I am unsure of
+                # their unicode value
+                if event.keyCode() is 36:
+                    character = "Enter"
+                elif event.keyCode() is 51:
+                    character = "Backspace"                    
                 self.key_hook(event.keyCode(), 
                               modifiers,
-                              event.charactersIgnoringModifiers(),
+                              keycodes.get(character,
+                                           character),
                               event.isARepeat())
             elif event.type() == NSMouseMoved:
                 self.mouse_move_hook(loc.x, loc.y)
@@ -108,7 +120,80 @@ class SniffCocoa:
             AppHelper.stopEventLoop()
             raise
 
-if __name__ == '__main__':
-    sc = SniffCocoa()
-    sc.run()
-
+# Cocoa does not provide a good api to get the keycodes, therefore we
+# have to provide our own.
+keycodes = {
+   u"\u0009" : "Tab",
+   u"\u001b" : "Escape",
+   u"\uf700" : "Up",
+   u"\uF701" : "Down",
+   u"\uF702" : "Left",
+   u"\uF703" : "Right",
+   u"\uF704" : "F1",
+   u"\uF705" : "F2",
+   u"\uF706" : "F3",
+   u"\uF707" : "F4",
+   u"\uF708" : "F5",
+   u"\uF709" : "F6",
+   u"\uF70A" : "F7",
+   u"\uF70B" : "F8",
+   u"\uF70C" : "F9",
+   u"\uF70D" : "F10",
+   u"\uF70E" : "F11",
+   u"\uF70F" : "F12",
+   u"\uF710" : "F13",
+   u"\uF711" : "F14",
+   u"\uF712" : "F15",
+   u"\uF713" : "F16",
+   u"\uF714" : "F17",
+   u"\uF715" : "F18",
+   u"\uF716" : "F19",
+   u"\uF717" : "F20",
+   u"\uF718" : "F21",
+   u"\uF719" : "F22",
+   u"\uF71A" : "F23",
+   u"\uF71B" : "F24",
+   u"\uF71C" : "F25",
+   u"\uF71D" : "F26",
+   u"\uF71E" : "F27",
+   u"\uF71F" : "F28",
+   u"\uF720" : "F29",
+   u"\uF721" : "F30",
+   u"\uF722" : "F31",
+   u"\uF723" : "F32",
+   u"\uF724" : "F33",
+   u"\uF725" : "F34",
+   u"\uF726" : "F35",
+   u"\uF727" : "Insert",
+   u"\uF728" : "Delete",
+   u"\uF729" : "Home",
+   u"\uF72A" : "Begin",
+   u"\uF72B" : "End",
+   u"\uF72C" : "PageUp",
+   u"\uF72D" : "PageDown",
+   u"\uF72E" : "PrintScreen",
+   u"\uF72F" : "ScrollLock",
+   u"\uF730" : "Pause",
+   u"\uF731" : "SysReq",
+   u"\uF732" : "Break",
+   u"\uF733" : "Reset",
+   u"\uF734" : "Stop",
+   u"\uF735" : "Menu",
+   u"\uF736" : "User",
+   u"\uF737" : "System",
+   u"\uF738" : "Print",
+   u"\uF739" : "ClearLine",
+   u"\uF73A" : "ClearDisplay",
+   u"\uF73B" : "InsertLine",
+   u"\uF73C" : "DeleteLine",
+   u"\uF73D" : "InsertChar",
+   u"\uF73E" : "DeleteChar",
+   u"\uF73F" : "Prev",
+   u"\uF740" : "Next",
+   u"\uF741" : "Select",
+   u"\uF742" : "Execute",
+   u"\uF743" : "Undo",
+   u"\uF744" : "Redo",
+   u"\uF745" : "Find",
+   u"\uF746" : "Help",
+   u"\uF747" : "ModeSwitch" }

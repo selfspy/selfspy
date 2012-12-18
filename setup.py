3597a14c556681f97d2ae99669a8ce601a1cf7f5
David Fendrich <david@aitellu.com>
2012-12-18 16:47:36 +0100

    Fixed requirements for Linux

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import os
 import platform
+dep_link = []
 if platform.system() == 'Darwin':
     req_file = 'osx-requirements.txt'
 else:
     req_file = 'requirements.txt'
-with open(os.path.join(os.path.dirname(__file__), req_file)) as f:
-    requires = f.readlines()
+    dep_link = 'svn+https://python-xlib.svn.sourceforge.net/svnroot/python-xlib/tags/xlib_0_15rc1/#egg=pyxlib'
 
+with open(os.path.join(os.path.dirname(__file__), req_file)) as f:
+    requires = ['pyxlib' if 'python-xlib' in s else s for s in f.readlines()]
+    
+
+print '"%s"' % requires
+    
 from setuptools import setup
 
 
